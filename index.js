d6451974df4a8333cbbce7654f3c5f6ef6800dfc
hubaib65 <shoaibhubaib@gmail.com>
2024-03-29 23:37:32 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        message: "Enter first number: ",
+        type: "number",
+        name: "num1",
+    },
+    {
+        message: "Enter second number: ",
+        type: "number",
+        name: "num2",
+    },
+    { message: "select any operator of your choice: ",
+        type: "list",
+        name: "operator",
+        choices: ["add", "sub", "mul", "div"] },
+]);
+if (answers.operator === "add") {
+    console.log("Your result is = ", answers.num1 + answers.num2);
+}
+else if (answers.operator === "sub") {
+    console.log("Your result is = ", answers.num1 - answers.num2);
+}
+else if (answers.operator === "mul") {
+    console.log("Your result is = ", answers.num1 * answers.num2);
+}
+else if (answers.operator === "div") {
+    console.log("Your result is = ", answers.num1 / answers.num2);
+}
+else {
+    console.log("invalid operator");
+}
